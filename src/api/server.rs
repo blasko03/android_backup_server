f95b5479c68936703d87d99364c3228e72093099
Daniel Blasina <blzk100@gmail.om>
2026-06-24 18:51:23 +0200

    remove openssl

--- src/api/server.rs
+++ src/api/server.rs
@@ -8,18 +8,6 @@ use openssl::ssl::{SslAcceptor, SslFiletype, SslMethod};
 
 #[actix_web::main]
 pub async fn start_server() -> std::io::Result<()> {
-    let mut builder = SslAcceptor::mozilla_intermediate(SslMethod::tls()).unwrap();
-    builder
-        .set_private_key_file(
-            "/home/daniel/apps/certs/danielblasinaCA/issued/backups.danielblasina.dev.key",
-            SslFiletype::PEM,
-        )
-        .unwrap();
-    builder
-        .set_certificate_chain_file(
-            "/home/daniel/apps/certs/danielblasinaCA/issued/backups.danielblasina.dev.crt",
-        )
-        .unwrap();
     let port = std::env::var("PORT")
         .unwrap_or_default()
         .parse::<u16>()
@@ -36,7 +24,6 @@ pub async fn start_server() -> std::io::Result<()> {
             .service(files_list)
             .service(login)
     })
-    .bind_openssl("127.0.0.1:9090", builder)?
     .bind(("0.0.0.0", port))?
     .run()
     .await
